f561375a704b40b24e066a9d4b71f4ef35a4e1b8
KrypTikzz <rojasrodrigo95@gmail.com>
2026-05-19 19:28:09 -0400

    fix app

--- PichangApp/msvc-usuario/src/main/java/com/PichangApp/msvc_usuario/MsvcUsuarioApplication.java
+++ PichangApp/msvc-usuario/src/main/java/com/PichangApp/msvc_usuario/MsvcUsuarioApplication.java
@@ -3,7 +3,10 @@ package com.PichangApp.msvc_usuario;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+
 @SpringBootApplication
+@EnableJpaRepositories(basePackages = "com.PichangApp.msvc_usuario.repositories")
 public class MsvcUsuarioApplication {
 
 	public static void main(String[] args) {
