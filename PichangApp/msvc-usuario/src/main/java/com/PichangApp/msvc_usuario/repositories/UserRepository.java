75641d5904b40790fef398946c3b8818a6c8e7d6
KrypTikzz <rojasrodrigo95@gmail.com>
2026-05-19 19:29:04 -0400

    Repository

--- PichangApp/msvc-usuario/src/main/java/com/PichangApp/msvc_usuario/repositories/UserRepository.java
+++ PichangApp/msvc-usuario/src/main/java/com/PichangApp/msvc_usuario/repositories/UserRepository.java
@@ -1,6 +1,6 @@
-package com.PichangApp.msvc.usuario.repositories;
+package com.PichangApp.msvc_usuario.repositories;
 
-import com.PichangApp.msvc.usuario.models.entities.User;
+import com.PichangApp.msvc_usuario.models.entities.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
