b0ccf63074b3f2430cf2cba3c05f54b1f8cdf9a1
KrypTikzz <rojasrodrigo95@gmail.com>
2026-05-19 19:27:43 -0400

    v1

--- PichangApp/msvc-usuario/src/main/java/com/PichangApp/msvc_usuario/security/services/JpaUserDetailsService.java
+++ PichangApp/msvc-usuario/src/main/java/com/PichangApp/msvc_usuario/security/services/JpaUserDetailsService.java
@@ -1,7 +1,7 @@
-package com.PichangApp.msvc.usuario.security.services;
+package com.PichangApp.msvc_usuario.security.services;
 
-import com.PichangApp.msvc.usuario.models.entities.User;
-import com.PichangApp.msvc.usuario.repositories.UserRepository;
+import com.PichangApp.msvc_usuario.models.entities.User;
+import com.PichangApp.msvc_usuario.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
