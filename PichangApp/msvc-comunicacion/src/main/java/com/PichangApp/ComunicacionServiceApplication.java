c97fd140a2d11f769ffaedfc25ea195584ab897f
KrypTikzz <rojasrodrigo95@gmail.com>
2026-05-20 17:42:10 -0400

    fix comunicacion

--- PichangApp/msvc-comunicacion/src/main/java/com/PichangApp/ComunicacionServiceApplication.java
+++ PichangApp/msvc-comunicacion/src/main/java/com/PichangApp/ComunicacionServiceApplication.java
@@ -2,8 +2,10 @@ package com.PichangApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableFeignClients(basePackages = "com.PichangApp.feign")
 public class ComunicacionServiceApplication {
 
 	public static void main(String[] args) {
