8042ca343d5aa620222bdb44801204bef4b32f19
lucmachuca <luc.machuca@duocuc.cl>
2026-06-19 13:12:08 -0400

    Reemplaza Autowired por inyección por constructor y elimina anotaciones Repository redundantes en msvc-usuario

--- PichangApp/msvc-usuario/src/main/java/com/PichangApp/msvc_usuario/repositories/RoleRepository.java
+++ PichangApp/msvc-usuario/src/main/java/com/PichangApp/msvc_usuario/repositories/RoleRepository.java
@@ -2,11 +2,8 @@ package com.PichangApp.msvc_usuario.repositories;
 
 import com.PichangApp.msvc_usuario.models.entities.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
 import java.util.Optional;
 
-@Repository
 public interface RoleRepository extends JpaRepository<Role, Long> {
     Optional<Role> findByName(String name);
 }
--- PichangApp/msvc-usuario/src/main/java/com/PichangApp/msvc_usuario/security/services/JpaUserDetailsService.java
+++ PichangApp/msvc-usuario/src/main/java/com/PichangApp/msvc_usuario/security/services/JpaUserDetailsService.java
@@ -18,8 +18,11 @@ import java.util.stream.Collectors;
 @Service
 public class JpaUserDetailsService implements UserDetailsService {
 
-    @Autowired
-    private UserRepository userRepository;
+    private final UserRepository userRepository;
+
+    public JpaUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
 
     @Override
     @Transactional(readOnly = true)
