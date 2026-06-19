842eef3e017dad6e5df48fa3f649f1a2e1cc1330
KrypTikzz <rojasrodrigo95@gmail.com>
2026-06-19 19:22:58 -0400

    dto y feign

--- PichangApp/msvc-match/src/main/java/com/PichangApp/dto/MatchSocialResponse.java
+++ PichangApp/msvc-match/src/main/java/com/PichangApp/dto/MatchSocialResponse.java
@@ -3,9 +3,10 @@ package com.PichangApp.dto;
 import java.time.LocalDateTime;
 
 public record MatchSocialResponse(
-                Long id,
-                Long usuarioAId,
-                Long usuarioBId,
-                boolean activo,
-                LocalDateTime fechaCreacion) {
+        Long id,
+        Long usuarioAId,
+        Long usuarioBId,
+        Boolean activo,
+        LocalDateTime fechaCreacion
+) {
 }
