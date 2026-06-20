595a899df30e9c73220181ae0ae1a2d455c0c9d9
KrypTikzz <rojasrodrigo95@gmail.com>
2026-06-19 20:21:30 -0400

    Agrega evento RabbitMQ para notificar mensajes enviados

--- PichangApp/msvc-comunicacion/src/main/java/com/PichangApp/dto/MensajeCreadoEvent.java
+++ PichangApp/msvc-comunicacion/src/main/java/com/PichangApp/dto/MensajeCreadoEvent.java
@@ -1,4 +1,14 @@
 package com.PichangApp.dto;
 
-public class MensajeCreadoEvent {
+import java.time.LocalDateTime;
+
+public record MensajeCreadoEvent(
+        Long mensajeId,
+        Long salaId,
+        Long matchSocialId,
+        Long remitenteId,
+        Long destinatarioId,
+        String contenido,
+        LocalDateTime fechaEnvio
+) {
 }
