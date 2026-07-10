9c917cba6554b169e206cbede2a8c5cf95f15fd4
KrypTikzz <rojasrodrigo95@gmail.com>
2026-07-09 20:39:17 -0400

    arreglo de test

--- PichangApp/msvc-usuario/src/main/java/com/PichangApp/msvc_usuario/models/dtos/UpdateUserProfileDTO.java
+++ PichangApp/msvc-usuario/src/main/java/com/PichangApp/msvc_usuario/models/dtos/UpdateUserProfileDTO.java
@@ -1,14 +1,13 @@
 package com.PichangApp.msvc_usuario.models.dtos;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+import java.util.HashMap;
 import java.util.Map;
 
 @Data
 @NoArgsConstructor
-@AllArgsConstructor
 public class UpdateUserProfileDTO {
 
     private String descripcion;
@@ -18,4 +17,49 @@ public class UpdateUserProfileDTO {
     private Map<String, Object> atributosDeportivos;
     private Double latitud;
     private Double longitud;
+
+    public UpdateUserProfileDTO(
+            String descripcion,
+            Integer edad,
+            String sexo,
+            String deportePrincipal,
+            Map<String, ?> atributosDeportivos,
+            Double latitud,
+            Double longitud
+    ) {
+        this.descripcion = descripcion;
+        this.edad = edad;
+        this.sexo = sexo;
+        this.deportePrincipal = deportePrincipal;
+        this.atributosDeportivos = copiarAtributos(atributosDeportivos);
+        this.latitud = latitud;
+        this.longitud = longitud;
+    }
+
+    public UpdateUserProfileDTO(
+            String descripcion,
+            Integer edad,
+            String deportePrincipal,
+            Map<String, ?> atributosDeportivos,
+            Double latitud,
+            Double longitud
+    ) {
+        this(
+                descripcion,
+                edad,
+                null,
+                deportePrincipal,
+                atributosDeportivos,
+                latitud,
+                longitud
+        );
+    }
+
+    private Map<String, Object> copiarAtributos(Map<String, ?> atributos) {
+        if (atributos == null) {
+            return null;
+        }
+
+        return new HashMap<>(atributos);
+    }
 }
